bc0a595dc82ab3aab3c85bdfc10030f077583694
Matthew C <m23.cullen@gmail.com>
2019-10-11 20:16:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, 
-  -(projects.funding_goal - SUM(pledges.amount)
+  (projects.funding_goal - SUM(pledges.amount) * -1
   FROM projects
   JOIN pledges 
   ON projects.id = pledges.project_id 
