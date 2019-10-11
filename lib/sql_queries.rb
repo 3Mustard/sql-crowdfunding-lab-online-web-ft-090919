a632a72c79d1b3444b019b65e5c23072a29dd6fa
Matthew C <m23.cullen@gmail.com>
2019-10-11 19:09:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,6 +3,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   FROM projects 
   JOIN pledges 
   ON projects.id = pledge.project_id 
+  
   ORDER BY title;"
 end
 
